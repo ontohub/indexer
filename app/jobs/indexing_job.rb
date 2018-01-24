20d9cf553c941b4aaefa08ffa5672d43aefaeb6f
Daniel Hackbarth <da_ha@uni-bremen.de>
2018-01-24 15:12:35 +0100

    Wip tests

--- app/jobs/indexing_job.rb
+++ app/jobs/indexing_job.rb
@@ -3,8 +3,15 @@
 # Post-processes a finished job that was sent to Hets
 class IndexingJob < ApplicationJob
   queue_as "#{Settings.rabbitmq.prefix}_indexing"
-
+  ALLOWED_INDEXERS = [RepositoryIndexer,
+                      UserIndexer,
+                      OrganizationIndexer].map(&:to_s)
   def perform(job)
-    "#{job['class']}Indexer".constantize.update(job['id'])
+    indexer_class = "#{job['class']}Indexer"
+    if ALLOWED_INDEXERS.include?(indexer_class)
+      indexer_class.constantize.update(job['id'])
+    else
+      raise "Invalid class argument: '#{job['class']}'"
+    end
   end
 end
