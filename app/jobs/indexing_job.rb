1c1626939500d3e44d331cdf4bd29a25b0fd77bc
Eugen Kuksa <kuksa.eugen@gmail.com>
2018-02-08 22:00:53 +0100

    Replace prefix and exchange settings by virtual host. (#8)

--- app/jobs/indexing_job.rb
+++ app/jobs/indexing_job.rb
@@ -2,7 +2,7 @@
 
 # Post-processes a finished job that was sent to Hets
 class IndexingJob < ApplicationJob
-  queue_as "#{Settings.rabbitmq.prefix}_indexing"
+  queue_as 'indexing'
   ALLOWED_INDEXERS = [RepositoryIndexer,
                       UserIndexer,
                       OrganizationIndexer].map(&:to_s)
